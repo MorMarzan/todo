0b88e2437b277ae28ed8af1e94006b02c5a6523c
Mor Marzan <mormarzan@gmail.com>
2023-12-23 10:22:40 +0200

    Add style

--- cmps/TodoPreview.jsx
+++ cmps/TodoPreview.jsx
@@ -1,12 +1,13 @@
 
 
-export function TodoPreview({todo}) {
-// export function TodoPreview({ todo }) {
+export function TodoPreview({ todo }) {
+
+    const dynClass = todo.isDone ? 'done' : ''
+
     return (
         <article className="todo-preview">
-            <h2>{todo.txt}</h2>
-            <h4>id: {todo._id}</h4>
-            <h4>Todo done? {todo.isDone ? 'yes': 'no'}</h4>
+            <p className={dynClass}>{todo.txt}</p>
+            {/* <h4>Todo done? {todo.isDone ? 'yes' : 'no'}</h4> */}
         </article>
     )
 }
